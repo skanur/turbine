13b723df1e54c9c1eea96990af544eb339f91069
Sudeep Kanur <skanur@abo.fi>
2019-06-01 10:41:32 +0300

    fix: Use np lcm instead

--- Turbine/calc/lcm.py
+++ Turbine/calc/lcm.py
@@ -2,8 +2,12 @@ from __future__ import division
 from past.utils import old_div
 from math import gcd
 
+import numpy as np
+
+# def lcm(a, b):
+#     return old_div(abs(a * b), gcd(a, b))
 def lcm(a, b):
-    return old_div(abs(a * b), gcd(a, b))
+    np.lcm(np.int64(a), np.int64(b), dtype=int64)
 
 
 def lcm_list(l):
